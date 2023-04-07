d3081b9e85211f0bedd00c107feb6371cc373170
Vitalii Komenda <vitaliikomenda@Vitaliis-MacBook-Air.local>
2023-04-07 16:58:28 +0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import bodyParser from "body-parser";
 import { sequelize } from "./model";
 import todoRouter from "./routes/todo";
 import authRouter from "./routes/auth";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({ credentials: true, origin: true }));
 app.use(bodyParser.json());
 app.set("sequelize", sequelize);
 app.set("models", sequelize.models);
